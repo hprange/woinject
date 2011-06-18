f810195e2ed1796508390c2f3e9040204b7a07c0
Henrique Prange <hprange@gmail.com>
2011-06-18 20:07:32 -0300

    More experimental code to hack _NSUtilities instantiation methods

--- src/main/java/com/woinject/Runner.java
+++ src/main/java/com/woinject/Runner.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010 hprange <hprange@gmail.com>
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,6 +20,11 @@ import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.Loader;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.webobjects.appserver.WOSession;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+
 /**
  * Experiment to avoid inheritance of Application, Session, DirectAction and
  * EnterpriseObject classes.
@@ -27,18 +32,53 @@ import javassist.Loader;
  * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
  * @since 1.0
  */
-public class Runner
-{
-	public static void main(String[] args) throws Throwable
-	{
-		ClassPool cp = ClassPool.getDefault();
-		Loader cl = new Loader(cp);
-		CtClass cc = cp.get("com.webobjects.foundation._NSUtilities");
-		CtMethod m = cc.getDeclaredMethod("instantiateObject");
-		m.insertBefore("{ System.out.println(\"\tClass: \"+$1+\" Parameters: \"+java.util.Arrays#toString($2)+\"\"); }");
+public abstract class Runner {
+    private static Injector injector;
 
-		Thread.currentThread().setContextClassLoader(cl);
-
-		cl.run("br.com.doit.Application", args);
+    private static Injector injector() {
+	if (injector == null) {
+	    injector = Guice.createInjector(new WOInjectModule());
 	}
+
+	return injector;
+    }
+
+    public static Object intercept(Class<?> clazz, Object[] parameters) {
+	System.out.println("\tClass: " + clazz + " Parameters: " + java.util.Arrays.toString(parameters));
+
+	if (WOSession.class.isAssignableFrom(clazz)) {
+	    System.out.println("Creating an instace of " + clazz.getName());
+
+	    Object instance = injector().getInstance(clazz);
+
+	    return instance;
+	}
+
+	if (EOEnterpriseObject.class.isAssignableFrom(clazz)) {
+	    System.out.println("Creating an instace of " + clazz.getName());
+
+	    Object instance = injector().getInstance(clazz);
+
+	    return instance;
+	}
+
+	return null;
+    }
+
+    public static void main(String[] args) throws Throwable {
+	ClassPool cp = ClassPool.getDefault();
+	Loader cl = new Loader(cp);
+	CtClass cc = cp.get("com.webobjects.foundation._NSUtilities");
+	CtMethod m = cc.getDeclaredMethod("instantiateObject");
+
+	m.insertBefore("{ Object result = com.woinject.Runner.intercept($1, $2); if(result!=null) return result; }");
+
+	m = cc.getDeclaredMethod("instantiateObjectWithConstructor");
+
+	m.insertBefore("{ Object result = com.woinject.Runner.intercept($2, $3); if(result!=null) return result; }");
+
+	Thread.currentThread().setContextClassLoader(cl);
+
+	cl.run("com.woinject.app.Application", args);
+    }
 }
--- src/main/java/com/woinject/WOInjectModule.java
+++ src/main/java/com/woinject/WOInjectModule.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010 hprange <hprange@gmail.com>
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,18 +17,19 @@
 package com.woinject;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 /**
  * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
  * @since 1.0
  */
-public class WOInjectModule extends AbstractModule
-{
+public class WOInjectModule extends AbstractModule {
 
-	@Override
-	protected void configure()
-	{
-		bindScope(WOSessionScoped.class, new WOSessionScope());
-	}
+    @Override
+    protected void configure() {
+	bindScope(WOSessionScoped.class, new WOSessionScope());
 
+	// TODO: Remove. It's just here just to help debugging.
+	bind(String.class).annotatedWith(Names.named("test")).toInstance("yes");
+    }
 }
