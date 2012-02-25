1cfb3df5d0722eac5155def2c3d04b8a01e6877c
Henrique Prange <hprange@gmail.com>
2012-02-25 21:57:18 -0200

    Add some basic javadoc information

---
+++ src/main/java/com/webobjects/foundation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes able to create objects and inject using Guice.
+ * 
+ * @since 1.0
+ */
+package com.webobjects.foundation;
+
---
+++ src/main/java/com/woinject/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all the base classes related to WOInject core features.
+ * 
+ * @since 1.0
+ */
+package com.woinject;
+
