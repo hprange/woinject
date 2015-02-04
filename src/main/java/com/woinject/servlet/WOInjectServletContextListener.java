8bf0ced0518cc2966d97480caf5a17f8de92d100
Henrique Prange <hprange@gmail.com>
2015-02-04 15:43:25 -0200

    Transform the WOInjectServlet into the WOInjectServletContextListener (closes #12)

--- src/main/java/com/woinject/servlet/WOInjectServletContextListener.java
+++ src/main/java/com/woinject/servlet/WOInjectServletContextListener.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010 hprange <hprange@gmail.com>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -22,28 +22,33 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.woinject.WOInject;
 
 /**
- * The <code>WOInjectServlet</code> initializes the WOInject framework in the
- * context of servlet applications.
+ * The <code>WOInjectServletContextListener</code> initializes the WOInject
+ * framework in the context of servlet applications. It's important to
+ * initialize WOInject very early in the context of a servlet application.
+ * Remember to add the listener in the first place on the web.xml descriptor.
  * <p>
  * Add the following snippet into the web.xml:
  * <p>
  *
  * <pre>
- * 	&lt;servlet&gt;
- * 		&lt;servlet-name&gt;WOInjectServlet&lt;/servlet-name&gt;
- * 		&lt;servlet-class&gt;com.woinject.servlet.WOInjectServlet&lt;/servlet-class&gt;
- * 		&lt;load-on-startup&gt;1&lt;/load-on-startup&gt;
- * 	&lt;/servlet&gt;
+ * 	&lt;listener&gt;
+ * 		&lt;listener-class&gt;com.woinject.servlet.WOInjectServletContextListener&lt;/listener-class&gt;
+ * 	&lt;/listener&gt;
  * </pre>
  *
  * @author <a href="mailto:hprange@gmail.com.br">Henrique Prange</a>
- * @since 1.1
+ * @since 1.2
  */
-public class WOInjectServlet extends HttpServlet {
+public class WOInjectServletContextListener implements ServletContextListener {
+    private static final String WOINJECT_MARKER_ATTRIBUTE = WOInject.class.getName();
+
     private static Class<?> loadClass(ClassLoader loader, String classname, byte[] bytes) {
 	Class<?> clazz = null;
 
@@ -69,18 +74,34 @@ public class WOInjectServlet extends HttpServlet {
     }
 
     /**
-     * Load the changed <code>_NSUtilities</code> class required by WOInject
-     * during the container initialization.
+     * Remove the WOInject marker associated with this context.
      *
-     * @see javax.servlet.GenericServlet#init()
+     * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.
+     *      ServletContextEvent)
      */
-    @Override
-    public void init() throws ServletException {
-	super.init();
+    public void contextDestroyed(ServletContextEvent event) {
+	ServletContext servletContext = event.getServletContext();
+
+	servletContext.removeAttribute(WOINJECT_MARKER_ATTRIBUTE);
+    }
+
+    /**
+     * Load the changed <code>_NSUtilities</code> class required by WOInject
+     * during the context initialization.
+     *
+     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent event) {
+	ServletContext context = event.getServletContext();
+
+	if (context.getAttribute(WOINJECT_MARKER_ATTRIBUTE) != null) {
+	    // WOInject has already been initialized in this context
+	    return;
+	}
 
 	ClassPool pool = ClassPool.getDefault();
 
-	pool.insertClassPath(new ClassClassPath(WOInjectServlet.class));
+	pool.insertClassPath(new ClassClassPath(WOInjectServletContextListener.class));
 
 	try {
 	    String classname = "com.webobjects.foundation._NSUtilities";
@@ -101,5 +122,7 @@ public class WOInjectServlet extends HttpServlet {
 	} catch (Throwable exception) {
 	    throw new Error("Cannot initialize the application to take advantage of WOInject features.", exception);
 	}
+
+	context.setAttribute(WOINJECT_MARKER_ATTRIBUTE, new Object());
     }
 }
